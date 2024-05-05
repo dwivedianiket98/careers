8caaa7c6cbdc6e42b2fdc60cc7e0c65839fbd571
Aniket Dwivedi <dwivedianiket98@gmail.com>
2024-05-05 23:27:20 +0530

    UserFlow - adding user manager for usercontroller remaining methods

--- src/main/java/com/interscope/careers/manager/UserManager.java
+++ src/main/java/com/interscope/careers/manager/UserManager.java
@@ -1,8 +1,25 @@
 package com.interscope.careers.manager;
 
+import com.interscope.careers.model.CandidateResponseModel;
+import com.interscope.careers.model.RecruiterResponseModel;
 import com.interscope.careers.model.UserResponseModel;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 public interface UserManager {
     UserResponseModel createUser(String name, String email, String password, Integer age, String address, Integer roleId, MultipartFile resume);
+
+    UserResponseModel getCandidateById(Integer id);
+
+    List<RecruiterResponseModel> getRecruiters();
+
+    List<CandidateResponseModel> getCandidates();
+
+    List<UserResponseModel> getUsers();
+
+    ResponseEntity<Resource> downloadResume(Integer id);
 }
