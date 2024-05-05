b869b60fe574fb85c9d535af8624eb309b3f26d7
Aniket Dwivedi <dwivedianiket98@gmail.com>
2024-05-05 18:03:31 +0530

    Updating models - removed getter setters for lombok

--- src/main/java/com/interscope/careers/model/RecruiterResponseModel.java
+++ src/main/java/com/interscope/careers/model/RecruiterResponseModel.java
@@ -9,49 +9,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class RecruiterResponseModel {
 	private String userName;
+	private String email;
 	private Integer userAge;
 	private String address;
-	private String email;
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getUserName() {
-		return userName;
-	}
-
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-
-	public Integer getUserAge() {
-		return userAge;
-	}
-
-	public void setUserAge(Integer userAge) {
-		this.userAge = userAge;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public RecruiterResponseModel(String userName, String email, Integer userAge, String address) {
-		super();
-		this.userName = userName;
-		this.email = email;
-		this.userAge = userAge;
-		this.address = address;
-	}
-
 
 }
