196b35ddbde95914ac2f18e6877984af164fb63f
Aniket Dwivedi <dwivedianiket98@gmail.com>
2024-05-14 20:49:39 +0530

    2-Security implementing bcrypt password encoder

--- src/main/java/com/interscope/careers/manager/impl/UserManagerImpl.java
+++ src/main/java/com/interscope/careers/manager/impl/UserManagerImpl.java
@@ -13,6 +13,7 @@ import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
@@ -34,7 +35,7 @@ public class UserManagerImpl implements UserManager {
             System.out.println("[*] file type: " + resume.getContentType());
             System.out.println("[*] file name: " + StringUtils.cleanPath(resume.getOriginalFilename()));
 
-            User user = userService.save(name, email, password, age, address, roleId, resume);
+            User user = userService.save(name, email, new BCryptPasswordEncoder().encode(password), age, address, roleId, resume);
             // creating resume download url
             String resumeUrl = "";
             resumeUrl = ServletUriComponentsBuilder.fromCurrentContextPath().path("user/candidates/resume/")
@@ -43,7 +44,7 @@ public class UserManagerImpl implements UserManager {
             response = new CandidateResponseModel(user.getName(), user.getEmail(),
                     user.getAge(), user.getAddress(), resumeUrl, user.getId());
         } else if (roleId.equals(Roles.RECRUITER.getRoleId())) {
-            User user = userService.save(name, email, password, age, address, roleId);
+            User user = userService.save(name, email, new BCryptPasswordEncoder().encode(password), age, address, roleId);
 
             response = new RecruiterResponseModel(user.getName(), user.getEmail(),
                     user.getAge(), user.getAddress(), user.getId());
